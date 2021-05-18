6feac76e4ca999c5ec76cc9be2b7a118c2d4737d
Minota <34592028+xMinota@users.noreply.github.com>
2021-05-18 02:05:45 -0400

    :fire: Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+
+app.get('/', async (req, res) => {
+  res.send('Hello World')
+})
+
+const server = http.createServer(app)
+server.listen(process.env.PORT, () => {
+  console.log('SErver started listening on port ', process.env.PORT)
+})
